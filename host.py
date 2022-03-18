8a00d5e64b82bd3291a8268513371e417aeffa5a
Min Hyeok Lee <minhyeok.lee@mail.utoronto.ca>
2022-03-18 16:02:49 -0400

    initial commit

--- host.py
+++ host.py
@@ -12,11 +12,12 @@ class EndSystem:
     def receive(self, source_ip):
         pass
 '''
+#/usr/bin.python
 from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.util import dumpNodeConnections
 from mininet.log import setLogLevel
-
+'''
 net = Mininet()
 
 c0 = net.addController()
@@ -33,3 +34,29 @@ h1.setIP('192.168.1.2', 24)
 net.start
 net.pingAll()
 net.stop()
+'''
+
+class SingleSwitchTopo(Topo):
+    def __init__(self, n=2, **opts):
+        Topo.__init__(self, **opts)
+        switch = self.addSwitch('s1')
+
+        for h in range(n):
+            host = self.addHost('h%s'%(h+1))
+            self.addLink(host, switch)
+
+
+def simpleTest():
+    topo = SingleSwitchTopo(n=4)
+    net = Mininet(topo)
+    net.start()
+    print("dumping host connections")
+    dumpNodeConnections(net.hosts)
+    print("testing network connectivity")
+    net.pingAll()
+    net.stop()
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    simpleTest()
+
