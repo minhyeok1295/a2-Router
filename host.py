4e5318f2197eadfb2719b2d4f14fe41bb7f50fa9
Min Hyeok Lee <minhyeok.lee@mail.utoronto.ca>
2022-03-19 15:21:15 -0400

    pushed

--- host.py
+++ host.py
@@ -3,8 +3,8 @@ import socket
 
 #cmd line arg: python3 host.py ip
 
-def main(argv)
-    ip = argv[1]
+def main(argv):
+    ip = sys.argv[1]
     connection = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     connection.bind((ip,8000)) # host port 8000
     
@@ -14,6 +14,6 @@ def main(argv)
 
 
 if __name__ == "__main__":
-    main(sys.argv)
+    main()
     
     
