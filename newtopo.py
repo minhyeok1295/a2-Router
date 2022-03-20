f9f44d656a5c6fd23d627f341a519f79a04edcab
Min Hyeok Lee <minhyeok.lee@mail.utoronto.ca>
2022-03-19 22:51:08 -0400

    added s/c

--- newtopo.py
+++ newtopo.py
@@ -5,37 +5,112 @@ Created on Sat Mar 19 22:41:07 2022
 @author: MinHyeok
 """
 
-from mininet.topo import Topo
 from mininet.net import Mininet
-from mininet.util import dumpNodeConnections
+
+from mininet.node import Controller, RemoteController, OVSKernelSwitch, UserSwitch
+
+from mininet.cli import CLI
+
 from mininet.log import setLogLevel
 
-class SingleSwitchTopo(Topo):
-    
-    def __init__(self, n=2, **opts):
-        Topo.__init__(self, **opts)
-        s1 = self.addSwitch('s1')
-        s2 = self.addSwitch('s2')
-        h1 = self.addHost('h1')
-        h2 = self.addHost('h2')
-        h3 = self.addHost('h3')
-        h4 = self.addHost('h4')
-        r1 = self.addNode('r1')
-        #Add Links
-        self.addLink(h1,s1)
-        self.addLink(h2,s1)
-        self.addLink(h3,s2)
-        self.addLink(h4,s2)
-        self.addLink(s2,r1)
-        self.addLink(s1,r1)
-        
+from mininet.link import Link, TCLink
 
-if __name__ == "__main__":
-    setLogLevel('info')
-    
-    topo = SingleSwitchTopo(n=4)
-    net = Mininet(topo)
-    net.start()
-    dumpNodeConnections(net.hosts)
-    net.pingAll()
-    net.stop()
+ 
+
+def topology():
+
+        net = Mininet( controller=RemoteController, link=TCLink, switch=OVSKernelSwitch )
+
+ 
+
+        # Add hosts and switches
+
+        h1 = net.addHost( 'h1', ip="10.0.1.10/24", mac="00:00:00:00:00:01" )
+
+        h2 = net.addHost( 'h2', ip="10.0.2.10/24", mac="00:00:00:00:00:02" )
+
+        h3 = net.addHost( 'h3', ip="10.0.1.20/24", mac="00:00:00:00:00:03" )
+
+        h4 = net.addHost( 'h4', ip="10.0.2.20/24", mac="00:00:00:00:00:04" )
+
+        r1 = net.addHost( 'r1')
+
+        s1 = net.addSwitch( 's1')
+
+        s2 = net.addSwitch( 's2')
+
+        c0 = net.addController( 'c0', controller=RemoteController, ip='127.0.0.1', port=6633 )
+
+ 
+
+        net.addLink( r1, s1 )
+
+        net.addLink( r1, s2 )
+
+        net.addLink( h1, s1 )
+
+        net.addLink( h3, s1 )
+
+        net.addLink( h2, s2 )
+
+        net.addLink( h4, s2 )
+
+        net.build()
+
+        c0.start()
+
+        s1.start( [c0] )
+
+        s2.start( [c0] )
+
+        r1.cmd("ifconfig r1-eth0 0")
+
+        r1.cmd("ifconfig r1-eth1 0")
+
+        r1.cmd("ifconfig r1-eth0 hw ether 00:00:00:00:01:01")
+
+        r1.cmd("ifconfig r1-eth1 hw ether 00:00:00:00:01:02")
+
+        r1.cmd("ip addr add 10.0.1.1/24 brd + dev r1-eth0")
+
+        r1.cmd("ip addr add 10.0.2.1/24 brd + dev r1-eth1")
+
+        r1.cmd("echo 1 > /proc/sys/net/ipv4/ip_forward")
+
+        h1.cmd("ip route add default via 10.0.1.1")
+
+        h2.cmd("ip route add default via 10.0.2.1")
+
+        h3.cmd("ip route add default via 10.0.1.1")
+
+        h4.cmd("ip route add default via 10.0.2.1")
+
+        s1.cmd("ovs-ofctl add-flow s1 priority=1,arp,actions=flood")
+
+        s1.cmd("ovs-ofctl add-flow s1 priority=65535,ip,dl_dst=00:00:00:00:01:01,actions=output:1")
+
+        s1.cmd("ovs-ofctl add-flow s1 priority=10,ip,nw_dst=10.0.1.10,actions=output:2")
+
+        s1.cmd("ovs-ofctl add-flow s1 priority=10,ip,nw_dst=10.0.1.20,actions=output:3")
+
+        s2.cmd("ovs-ofctl add-flow s2 priority=1,arp,actions=flood")
+
+        s2.cmd("ovs-ofctl add-flow s2 priority=65535,ip,dl_dst=00:00:00:00:01:02,actions=output:1")
+
+        s2.cmd("ovs-ofctl add-flow s2 priority=10,ip,nw_dst=10.0.2.10,actions=output:2")
+
+        s2.cmd("ovs-ofctl add-flow s2 priority=10,ip,nw_dst=10.0.2.20,actions=output:3")
+
+
+        CLI( net )
+
+
+        net.stop()
+
+      
+
+if __name__ == '__main__':
+
+    setLogLevel( 'info' )
+
+    topology() 
