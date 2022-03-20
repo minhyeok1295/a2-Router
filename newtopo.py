1473bdddd1a5f6368d7ab33d448649aad65f267c
Min Hyeok Lee <minhyeok.lee@mail.utoronto.ca>
2022-03-20 00:32:53 -0400

    added s/c

--- newtopo.py
+++ newtopo.py
@@ -20,6 +20,7 @@ class NetworkTopo(Topo):
     def build(self, **_opts):
         # Add 2 routers in two different subnets
         r1 = self.addHost('r1', cls=LinuxRouter, ip='10.0.0.1/24')
+        r2 = self.addHost('r2', cls=LinuxRouter, ip='10.1.0.1/24')
 
         # Add 2 switches
         s1 = self.addSwitch('s1')
@@ -32,18 +33,25 @@ class NetworkTopo(Topo):
                      params2={'ip': '10.0.0.1/24'})
 
         self.addLink(s2,
-                     r1,
-                     intfName2='r1-eth2',
-                     params2={'ip': '10.0.0.1/24'})
+                     r2,
+                     intfName2='r2-eth1',
+                     params2={'ip': '10.1.0.1/24'})
+
+        # Add router-router link in a new subnet for the router-router connection
+        self.addLink(r1,
+                     r2,
+                     intfName1='r1-eth2',
+                     intfName2='r2-eth2',
+                     params1={'ip': '10.100.0.1/24'},
+                     params2={'ip': '10.100.0.2/24'})
 
         # Adding hosts specifying the default route
         d1 = self.addHost(name='d1',
                           ip='10.0.0.251/24',
                           defaultRoute='via 10.0.0.1')
-        
         d2 = self.addHost(name='d2',
-                          ip='10.0.0.252/24',
-                          defaultRoute='via 10.0.0.1')
+                          ip='10.1.0.252/24',
+                          defaultRoute='via 10.1.0.1')
 
         # Add host-switch links
         self.addLink(d1, s1)
@@ -55,7 +63,8 @@ def run():
     net = Mininet(topo=topo)
 
     # Add routing for reaching networks that aren't directly connected
-    info(net['r1'].cmd("ip route add 10.0.0.0/24 via 10.100.0.1 dev r1-eth1"))
+    info(net['r1'].cmd("ip route add 10.1.0.0/24 via 10.100.0.2 dev r1-eth2"))
+    info(net['r2'].cmd("ip route add 10.0.0.0/24 via 10.100.0.1 dev r2-eth2"))
 
     net.start()
     CLI(net)
--- topo.py
+++ topo.py
@@ -1,45 +1,66 @@
-
 from mininet.topo import Topo
 from mininet.net import Mininet
-from mininet.util import dumpNodeConnections
-from mininet.log import setLogLevel
-from mininet.util import irange
-# Reference http://csie.nqu.edu.tw/smallko/sdn/mininet_simple_router.html
+from mininet.node import Node
+from mininet.log import setLogLevel, info
+from mininet.cli import CLI
 
-class SimpleTopo(Topo):
 
-    def build(self):
-        # Simple Hosts, Swtiches and Router
+class LinuxRouter(Node):
+    def config(self, **params):
+        super(LinuxRouter, self).config(**params)
+        self.cmd('sysctl net.ipv4.ip_forward=1')
+
+    def terminate(self):
+        self.cmd('sysctl net.ipv4.ip_forward=0')
+        super(LinuxRouter, self).terminate()
+        
+        
+class NetworkTopo(Topo):
+    def build(self, **_opts):
+        # Add 2 routers in two different subnets
+        r1 = self.addHost('r1', cls=LinuxRouter, ip='10.0.0.1/24')
+
+        # Add 2 switches
         s1 = self.addSwitch('s1')
         s2 = self.addSwitch('s2')
-        '''
-        h1 = self.addHost('h1', ip='192.168.1.1/24', mac="00:00:00:00:00:01")
-        h2 = self.addHost('h2', ip='192.168.1.2/24', mac="00:00:00:00:00:02")
-        h3 = self.addHost('h3', ip='192.168.1.3/24', mac="00:00:00:00:00:03")
-        h4 = self.addHost('h4', ip='192.168.1.4/24', mac="00:00:00:00:00:04")
-        '''
-        h1 = self.addHost('h1')
-        h2 = self.addHost('h2')
-        h3 = self.addHost('h3')
-        h4 = self.addHost('h4')
-        r1 = self.addNode('r1')
-        #Add Links
-        self.addLink(h1,s1)
-        self.addLink(h2,s1)
-        self.addLink(h3,s2)
-        self.addLink(h4,s2)
-        self.addLink(s2,r1)
-        self.addLink(s1,r1)
-        
-topos  = {'mytopo':(lambda:SimpleTopo())}
 
-if __name__ == "__main__":
-    setLogLevel('info')
-    topo = SimpleTopo()
-    net = Mininet(topo)
+        # Add host-switch links in the same subnet
+        self.addLink(s1,
+                     r1,
+                     intfName2='r1-eth1',
+                     params2={'ip': '10.0.0.1/24'})
+
+        self.addLink(s2,
+                     r1,
+                     intfName2=-'r1-eth2',
+                     params2={'ip': '10.1.0.1/24'})
+
+
+        # Adding hosts specifying the default route
+        d1 = self.addHost(name='d1',
+                          ip='10.0.0.251/24',
+                          defaultRoute='via 10.0.0.1')
+        d2 = self.addHost(name='d2',
+                          ip='10.1.0.252/24',
+                          defaultRoute='via 10.1.0.1')
+
+        # Add host-switch links
+        self.addLink(d1, s1)
+        self.addLink(d2, s2)
+        
+def run():
+    topo = NetworkTopo()
+    net = Mininet(topo=topo)
+
+    # Add routing for reaching networks that aren't directly connected
+    info(net['r1'].cmd("ip route add 10.1.0.0/24 via 10.0.0.1 dev r1-eth2"))
+    info(net['r1'].cmd("ip route add 10.0.0.0/24 via 10.1.0.1 dev r1-eth1"))
+
     net.start()
-    print("Dumping host connections")
-    dumpNodeConnections(net.hosts)
-    print("testing network connectivity")
-    net.pingAll()
+    CLI(net)
     net.stop()
+
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    run()
