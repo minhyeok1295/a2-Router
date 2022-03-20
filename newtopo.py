09afab5bf11e266d94c8e8aaba177aa7d75c783d
Min Hyeok Lee <minhyeok.lee@mail.utoronto.ca>
2022-03-19 23:58:45 -0400

    added s/c

--- newtopo.py
+++ newtopo.py
@@ -55,7 +55,7 @@ def run():
     net = Mininet(topo=topo)
 
     # Add routing for reaching networks that aren't directly connected
-    info(net['r1'].cmd("ip route add 10.1.0.0/24 via 10.100.0.2 dev r1-eth2"))
+    info(net['r1'].cmd("ip route add 10.0.0.0/24 via 10.100.0.1 dev r1-eth1"))
 
     net.start()
     CLI(net)
