77d461d2440dbcaca82953e82252cdcdad883e3f
Min Hyeok Lee <minhyeok.lee@mail.utoronto.ca>
2022-03-19 22:48:14 -0400

    added s/c

--- newtopo.py
+++ newtopo.py
@@ -14,13 +14,21 @@ class SingleSwitchTopo(Topo):
     
     def __init__(self, n=2, **opts):
         Topo.__init__(self, **opts)
-        switch = self.addSwitch("s1")
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
+        h1 = self.addHost('h1')
+        h2 = self.addHost('h2')
+        h3 = self.addHost('h3')
+        h4 = self.addHost('h4')
+        r1 = self.addNode('r1')
+        #Add Links
+        self.addLink(h1,s1)
+        self.addLink(h2,s1)
+        self.addLink(h3,s2)
+        self.addLink(h4,s2)
+        self.addLink(s2,r1)
+        self.addLink(s1,r1)
         
-        
-        for h in range(n):
-            host = self.addHost('h%s'%(h+1))
-            self.addLink(host, switch)
-            
 
 if __name__ == "__main__":
     setLogLevel('info')
