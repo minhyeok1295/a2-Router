5bead87a71e2bc7081587cab0292f098426db20e
Min Hyeok Lee <minhyeok.lee@mail.utoronto.ca>
2022-03-22 03:21:35 -0400

    modified host

--- newtopo.py
+++ newtopo.py
@@ -68,7 +68,6 @@ def run():
     topo = NetworkTopo()
 
     net = Mininet(topo=topo)
-    net.pingAll()
     # Add routing for reaching networks that aren't directly connected
     #info(net['r1'].cmd("ip route add 10.1.0.0/24 via 10.0.0.1 dev r1-eth2"))
     #info(net['r1'].cmd("ip route add 10.0.0.0/24 via 10.1.0.1 dev r1-eth1"))
--- topo.py
+++ topo.py
@@ -1,85 +1,105 @@
-from mininet.topo import Topo
 from mininet.net import Mininet
-from mininet.node import Node
-from mininet.log import setLogLevel, info
+
+from mininet.node import Controller, RemoteController, OVSKernelSwitch, UserSwitch
+
 from mininet.cli import CLI
 
-topos = { 'mytopo': ( lambda: NetworkTopo() ) } 
+from mininet.log import setLogLevel
 
-class LinuxRouter(Node):
-    def config(self, **params):
-        super(LinuxRouter, self).config(**params)
-        self.cmd('sysctl net.ipv4.ip_forward=1')
+from mininet.link import Link, TCLink
 
-    def terminate(self):
-        self.cmd('sysctl net.ipv4.ip_forward=0')
-        super(LinuxRouter, self).terminate()
-        
-        
-class NetworkTopo(Topo):
-    def build(self, **_opts):
-        # Add 2 routers in two different subnets
-        r1 = self.addHost('r1', cls=LinuxRouter, ip='10.0.0.1/24')
+ 
 
-        # Add 2 switches
-        s1 = self.addSwitch('s1')
-        s2 = self.addSwitch('s2')
-        s3 = self.addSwitch('s3')
+def topology():
 
-        # Add host-switch links in the same subnet
-        self.addLink(s3,
-                     r1,
-                     intfName2='r1-eth1',
-                     params2={'ip': '10.0.0.1/24'})
+        net = Mininet( controller=RemoteController, link=TCLink, switch=OVSKernelSwitch )
 
-        self.addLink(s1,
-                     r1,
-                     intfName2='r1-eth2',
-                     params2={'ip': '172.168.0.1/24'})
+ 
 
-        self.addLink(s2,
-                     r1,
-                     intfName2='r1-eth3',
-                     params2={'ip': '192.168.1.1/24'})
-        
+        # Add hosts and switches
+
+        h1 = net.addHost( 'h1', ip="10.0.1.10/24", mac="00:00:00:00:00:01" )
+
+        h2 = net.addHost( 'h2', ip="10.0.2.10/24", mac="00:00:00:00:00:02" )
+
+        h3 = net.addHost( 'h3', ip="10.0.1.20/24", mac="00:00:00:00:00:03" )
+
+        h4 = net.addHost( 'h4', ip="10.0.2.20/24", mac="00:00:00:00:00:04" )
+
+        r1 = net.addHost( 'r1')
+
+        s1 = net.addSwitch( 's1')
+
+        s2 = net.addSwitch( 's2')
+
+        c0 = net.addController( 'c0', controller=RemoteController, ip='127.0.0.1', port=6633 )
+
+ 
+
+        net.addLink( r1, s1 )
+
+        net.addLink( r1, s2 )
+
+        net.addLink( h1, s1 )
+
+        net.addLink( h3, s1 )
+
+        net.addLink( h2, s2 )
+
+        net.addLink( h4, s2 )
+
+        net.build()
+
+        c0.start()
+
+        s1.start( [c0] )
+
+        s2.start( [c0] )
+
+        r1.cmd("ifconfig r1-eth0 0")
+
+        r1.cmd("ifconfig r1-eth1 0")
+
+        r1.cmd("ifconfig r1-eth0 hw ether 00:00:00:00:01:01")
+
+        r1.cmd("ifconfig r1-eth1 hw ether 00:00:00:00:01:02")
+
+        r1.cmd("ip addr add 10.0.1.1/24 brd + dev r1-eth0")
+
+        r1.cmd("ip addr add 10.0.2.1/24 brd + dev r1-eth1")
+
+        r1.cmd("echo 1 > /proc/sys/net/ipv4/ip_forward")
+
+        h1.cmd("ip route add default via 10.0.1.1")
+
+        h2.cmd("ip route add default via 10.0.2.1")
+
+        h3.cmd("ip route add default via 10.0.1.1")
+
+        h4.cmd("ip route add default via 10.0.2.1")
+
+        s1.cmd("ovs-ofctl add-flow s1 priority=1,arp,actions=flood")
+
+        s1.cmd("ovs-ofctl add-flow s1 priority=65535,ip,dl_dst=00:00:00:00:01:01,actions=output:1")
+
+        s1.cmd("ovs-ofctl add-flow s1 priority=10,ip,nw_dst=10.0.1.10,actions=output:2")
+
+        s1.cmd("ovs-ofctl add-flow s1 priority=10,ip,nw_dst=10.0.1.20,actions=output:3")
+
+        s2.cmd("ovs-ofctl add-flow s2 priority=1,arp,actions=flood")
+
+        s2.cmd("ovs-ofctl add-flow s2 priority=65535,ip,dl_dst=00:00:00:00:01:02,actions=output:1")
+
+        s2.cmd("ovs-ofctl add-flow s2 priority=10,ip,nw_dst=10.0.2.10,actions=output:2")
+
+        s2.cmd("ovs-ofctl add-flow s2 priority=10,ip,nw_dst=10.0.2.20,actions=output:3")
 
 
-        # Adding hosts specifying the default route
-        d1 = self.addHost(name='d1',
-                          ip='172.168.0.10/24',
-                          defaultRoute='via 172.168.0.1')
-        d2 = self.addHost(name='d2',
-                          ip='192.168.1.10/24',
-                          defaultRoute='via 192.168.1.1')
-        d3 = self.addHost(name='d3',
-                          ip='152.168.2.10/24',
-                          defaultRoute='via 152.168.2.1')
-        
-        d4 = self.addHost(name='d4',
-                          ip='132.168.3.10/24',
-                          defaultRoute='via 132.168.3.1')
-        # Add host-switch links
-        self.addLink(d1, s1)
-        self.addLink(d3, s1)
-        self.addLink(d2, s2)
-        self.addLink(d4, s2)
-        
-def run():
-    topo = NetworkTopo()
 
-    net = Mininet(topo=topo)
-    net.pingAll()
-    # Add routing for reaching networks that aren't directly connected
-    #info(net['r1'].cmd("ip route add 10.1.0.0/24 via 10.0.0.1 dev r1-eth2"))
-    #info(net['r1'].cmd("ip route add 10.0.0.0/24 via 10.1.0.1 dev r1-eth1"))
-
-    #net.start()
-    #CLI(net)
-    #net.stop()
-
+      
 
 if __name__ == '__main__':
-    setLogLevel('info')
-    #topo = { 'mytopo': ( lambda: NetworkTopo() ) } 
-    run()
-    
+
+    setLogLevel( 'info' )
+
+    topology() 
