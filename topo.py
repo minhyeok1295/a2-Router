a3c3f345ef2c9f45432f5e95a8c8b4f58855005a
Min Hyeok Lee <minhyeok.lee@mail.utoronto.ca>
2022-03-22 22:45:49 -0400

    topo changed

--- topo.py
+++ topo.py
@@ -24,33 +24,33 @@ class NetworkTopo(Topo):
         # Add 2 switches
         s1 = self.addSwitch('s1')
         s2 = self.addSwitch('s2')
-        s3 = self.addSwitch('s3')
+        #s3 = self.addSwitch('s3')
 
         # Add host-switch links in the same subnet
-        self.addLink(s3,
-                     r1,
-                     intfName2='r1-eth1',
-                     params2={'ip': '10.0.0.1/24'})
+        #self.addLink(s3,
+        #             r1,
+        #             intfName2='r1-eth1',
+        #            params2={'ip': '10.0.0.1/24'})
 
         self.addLink(s1,
                      r1,
-                     intfName2='r1-eth2',
-                     params2={'ip': '172.168.0.1/24'})
+                     intfName2='r1-eth0',
+                     params2={'ip': '10.0.0.1/24'})
 
         self.addLink(s2,
                      r1,
-                     intfName2='r1-eth3',
-                     params2={'ip': '192.168.1.1/24'})
+                     intfName2='r1-eth1',
+                     params2={'ip': '10.0.1.1/24'})
         
 
 
         # Adding hosts specifying the default route
         d1 = self.addHost(name='d1',
-                          ip='172.168.0.10/24',
-                          defaultRoute='via 172.168.0.1')
+                          ip='10.0.0.10/24',
+                          defaultRoute='via 10.0.0.1')
         d2 = self.addHost(name='d2',
                           ip='192.168.1.10/24',
-                          defaultRoute='via 192.168.1.1')
+                          defaultRoute='via 10.0.1.1')
 
         # Add host-switch links
         self.addLink(d1, s1)
