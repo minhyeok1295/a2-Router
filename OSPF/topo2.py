ee4679222af4cd9e45dc21f3fe74b2316be34afb
Min Hyeok Lee <minhyeok.lee@mail.utoronto.ca>
2022-03-31 13:01:26 -0400

    added topop

--- OSPF/topo2.py
+++ OSPF/topo2.py
@@ -23,12 +23,12 @@ class NetworkTopo(Topo):
         # Add 2 routers in two different subnets
         r1 = self.addHost('r1', cls=LinuxRouter, ip='10.0.0.1/24')
         r2 = self.addHost('r2', cls=LinuxRouter, ip='10.1.0.1/24')
-        r3 = self.addHost('r3', cls=LinuxRouter, ip='10.2.0.1/24')
+        #r3 = self.addHost('r3', cls=LinuxRouter, ip='10.2.0.1/24')
 
         # Add 2 switches
         s1 = self.addSwitch('s1')
         s2 = self.addSwitch('s2')
-        s3 = self.addSwitch('s3')
+        #s3 = self.addSwitch('s3')
 
         # Add host-switch links in the same subnet
         self.addLink(s1,
@@ -40,12 +40,12 @@ class NetworkTopo(Topo):
                      r2,
                      intfName2='r2-eth0',
                      params2={'ip': '10.1.0.1/24'})
-        
+        '''
         self.addLink(s3,
                      r3,
                      intfName2='r3-eth0',
                      params2={'ip': '10.2.0.1/24'})
-
+'''
         # Add router-router link in a new subnet for the router-router connection
         self.addLink(r1,
                      r2,
@@ -53,6 +53,7 @@ class NetworkTopo(Topo):
                      intfName2='r2-eth1',
                      params1={'ip': '10.100.0.1/24'},
                      params2={'ip': '10.100.0.2/24'})
+        '''
         self.addLink(r1,
                      r3,
                      intfName1='r1-eth2',
@@ -67,7 +68,7 @@ class NetworkTopo(Topo):
                      params1={'ip': '10.102.0.1/24'},
                      params2={'ip': '10.102.0.2/24'})
 
-
+        '''
         # Adding hosts specifying the default route
         d1 = self.addHost(name='d1',
                           ip='10.0.0.10/24',
@@ -76,14 +77,14 @@ class NetworkTopo(Topo):
                           ip='10.1.0.10/24',
                           defaultRoute='via 10.1.0.1')
         
-        d3 = self.addHost(name='d3',
-                          ip='10.2.0.10/24',
-                          defaultRoute='via 10.2.0.1')
+        #d3 = self.addHost(name='d3',
+        #                  ip='10.2.0.10/24',
+        #                  defaultRoute='via 10.2.0.1')
 
         # Add host-switch links
         self.addLink(d1, s1)
         self.addLink(d2, s2)
-        self.addLink(d3, s3)
+        #self.addLink(d3, s3)
 
 def run():
     topo = NetworkTopo()
