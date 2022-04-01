adfdf51147c534e69f8488c11fc48370a2c215f3
Min Hyeok Lee <minhyeok.lee@mail.utoronto.ca>
2022-04-01 11:18:46 -0400

    topo changed

--- OSPF/topo.py
+++ OSPF/topo.py
@@ -38,16 +38,16 @@ class NetworkTopo(Topo):
         self.addLink(sm, m, intfName2='m-eth0', params2={'ip': '10.0.0.1/24'})
         self.addLink(s1, r1, intfName2='r1-eth0', params2={'ip': '10.1.0.1/24'})
         self.addLink(s2, r2, intfName2='r2-eth0', params2={'ip': '10.2.0.1/24'})
-        #self.addLink(s2, r3, intfName2='r3-eth0', params2={'ip': '10.3.0.1/24'})
+        self.addLink(s2, r3, intfName2='r3-eth0', params2={'ip': '10.3.0.1/24'})
         
         # Add router-router link in a new subnet for the router-router connection
         self.addLink(r1, r2, intfName1='r1-eth2', intfName2='r2-eth2', params1={'ip': '10.11.0.1/24'}, params2={'ip': '10.11.0.2/24'})
-        #self.addLink(r1, r3, intfName1='r1-eth3', intfName2='r3-eth2', params1={'ip': '10.12.0.1/24'}, params2={'ip': '10.12.0.2/24'})
-        #self.addLink(r2, r3, intfName1='r2-eth3', intfName2='r3-eth3', params1={'ip': '10.13.0.1/24'}, params2={'ip': '10.13.0.2/24'})
+        self.addLink(r1, r3, intfName1='r1-eth3', intfName2='r3-eth2', params1={'ip': '10.12.0.1/24'}, params2={'ip': '10.12.0.2/24'})
+        self.addLink(r2, r3, intfName1='r2-eth3', intfName2='r3-eth3', params1={'ip': '10.13.0.1/24'}, params2={'ip': '10.13.0.2/24'})
         
         self.addLink(r1, m, intfName1='r1-eth1', intfName2='m-eth1', params1={'ip': '10.101.0.1/24'}, params2={'ip': '10.101.0.2/24'})
         self.addLink(r2, m, intfName1='r2-eth1', intfName2='m-eth2', params1={'ip': '10.102.0.1/24'}, params2={'ip': '10.102.0.2/24'})
-        #self.addLink(r3, m, intfName1='r3-eth1', intfName2='m-eth3', params1={'ip': '10.103.0.1/24'}, params2={'ip': '10.103.0.2/24'})
+        self.addLink(r3, m, intfName1='r3-eth1', intfName2='m-eth3', params1={'ip': '10.103.0.1/24'}, params2={'ip': '10.103.0.2/24'})
         
         
         # Adding hosts specifying the default route
@@ -57,7 +57,7 @@ class NetworkTopo(Topo):
         h3 = self.addHost(name='h3', ip='10.2.0.10/24', defaultRoute='via 10.2.0.1')
         h4 = self.addHost(name='h4', ip='10.2.0.20/24', defaultRoute='via 10.2.0.1')
         
-        #h5 = self.addHost(name='h5', ip='10.3.0.10/24', defaultRoute='via 10.3.0.1')
+        h5 = self.addHost(name='h5', ip='10.3.0.10/24', defaultRoute='via 10.3.0.1')
         
         
         
@@ -68,7 +68,7 @@ class NetworkTopo(Topo):
         self.addLink(h3, s2)
         self.addLink(h4, s2)
         
-        #self.addLink(h5, s3)
+        self.addLink(h5, s3)
 
 def run():
     topo = NetworkTopo()
@@ -82,21 +82,21 @@ def run():
     info(net['r1'].cmd("ip route add 10.0.0.0/24 via 10.101.0.2 dev r1-eth1"))
     
     info(net['r1'].cmd("ip route add 10.2.0.0/24 via 10.11.0.2 dev r1-eth2"))
-    #info(net['r1'].cmd("ip route add 10.3.0.0/24 via 10.12.0.2 dev r1-eth3"))
+    info(net['r1'].cmd("ip route add 10.3.0.0/24 via 10.12.0.2 dev r1-eth3"))
     
     info(net['r2'].cmd("ip route add 10.0.0.0/24 via 10.102.0.2 dev r2-eth1"))
     
     info(net['r2'].cmd("ip route add 10.1.0.0/24 via 10.11.0.1 dev r2-eth2"))
-    #info(net['r2'].cmd("ip route add 10.3.0.0/24 via 10.13.0.2 dev r2-eth3"))
+    info(net['r2'].cmd("ip route add 10.3.0.0/24 via 10.13.0.2 dev r2-eth3"))
     
-    #info(net['r3'].cmd("ip route add 10.0.0.0/24 via 10.103.0.2 dev r3-eth1"))  
+    info(net['r3'].cmd("ip route add 10.0.0.0/24 via 10.103.0.2 dev r3-eth1"))  
     
-    #info(net['r3'].cmd("ip route add 10.1.0.0/24 via 10.12.0.1 dev r3-eth2"))  
-    #info(net['r3'].cmd("ip route add 10.2.0.0/24 via 10.13.0.1 dev r3-eth3"))      
+    info(net['r3'].cmd("ip route add 10.1.0.0/24 via 10.12.0.1 dev r3-eth2"))  
+    info(net['r3'].cmd("ip route add 10.2.0.0/24 via 10.13.0.1 dev r3-eth3"))      
 
     info(net['m'].cmd("ip route add 10.1.0.0/24 via 10.101.0.1 dev m-eth1"))
     info(net['m'].cmd("ip route add 10.2.0.0/24 via 10.102.0.1 dev m-eth2"))
-    #info(net['m'].cmd("ip route add 10.3.0.0/24 via 10.103.0.1 dev m-eth3"))
+    info(net['m'].cmd("ip route add 10.3.0.0/24 via 10.103.0.1 dev m-eth3"))
     
     
     net.startTerms()
