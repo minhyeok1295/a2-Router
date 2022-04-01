9d7fe4af518e6ff2df0c5b690615d65463e2a1cb
Min Hyeok Lee <minhyeok.lee@mail.utoronto.ca>
2022-04-01 05:57:20 -0400

    topo changed

--- OSPF/topo.py
+++ OSPF/topo.py
@@ -32,77 +32,29 @@ class NetworkTopo(Topo):
         s3 = self.addSwitch('s3')
 
         # Add host-switch links in the same subnet
-        self.addLink(s1,
-                     r1,
-                     intfName2='r1-eth0',
-                     params2={'ip': '10.1.0.1/24'})
+        self.addLink(s1, r1, intfName2='r1-eth0', params2={'ip': '10.1.0.1/24'})
 
-        self.addLink(s2,
-                     r2,
-                     intfName2='r2-eth0',
-                     params2={'ip': '10.2.0.1/24'})
+        self.addLink(s2, r2, intfName2='r2-eth0', params2={'ip': '10.2.0.1/24'})
         
-        self.addLink(s3,
-                     r3,
-                     intfName2='r3-eth0',
-                     params2={'ip': '10.3.0.1/24'})
+        self.addLink(s3, r3, intfName2='r3-eth0', params2={'ip': '10.3.0.1/24'})
 
         #add router-monitor link
-        self.addLink(m, r1, intfName1='m-eth1', intfName2='r1-eth1', params1={'ip': '10.10.0.1/24'}, params2={'ip': '10.10.0.2/24'})
-        
-        self.addLink(m, r2, intfName1='m-eth1', intfName2='r2-eth1', params1={'ip': '10.10.0.3/24'}, params2={'ip': '10.10.0.4/24'})
-        
-        self.addLink(m, r3, intfName1='m-eth1', intfName2='r3-eth1', params1={'ip': '10.10.0.5/24'}, params2={'ip': '10.10.0.6/24'})
+        self.addLink(m, r1, intfName1='m-eth1', intfName2='r1-eth1', params1={'ip': '10.0.0.1/24'}, params2={'ip': '10.0.0.2/24'}) 
+        self.addLink(m, r2, intfName1='m-eth1', intfName2='r2-eth1', params1={'ip': '10.0.0.3/24'}, params2={'ip': '10.0.0.4/24'})       
+        self.addLink(m, r3, intfName1='m-eth1', intfName2='r3-eth1', params1={'p': '10.10.0.5/24'}, params2={'ip': '10.0.0.6/24'})        
         
         # Add router-router link in a new subnet for the router-router connection
-        self.addLink(r1,
-                     r2,
-                     intfName1='r1-eth2',
-                     intfName2='r2-eth2',
-                     params1={'ip': '10.100.0.1/24'},
-                     params2={'ip': '10.100.0.2/24'})
-        
-        self.addLink(r1,
-                     r3,
-                     intfName1='r1-eth3',
-                     intfName2='r3-eth2',
-                     params1={'ip': '10.101.0.1/24'},
-                     params2={'ip': '10.101.0.2/24'})
-        
-        
-        self.addLink(r2,
-                     r3,
-                     intfName1='r2-eth3',
-                     intfName2='r3-eth3',
-                     params1={'ip': '10.102.0.1/24'},
-                     params2={'ip': '10.102.0.2/24'})
+        self.addLink(r1, r2, intfName1='r1-eth2', intfName2='r2-eth2', params1={'ip': '10.100.0.1/24'}, params2={'ip': '10.100.0.2/24'})
+        self.addLink(r1, r3, intfName1='r1-eth3', intfName2='r3-eth2', params1={'ip': '10.101.0.1/24'}, params2={'ip': '10.101.0.2/24'})
+        self.addLink(r2, r3, intfName1='r2-eth3', intfName2='r3-eth3', params1={'ip': '10.102.0.1/24'}, params2={'ip': '10.102.0.2/24'})
         
         # Adding hosts specifying the default route
-        h1 = self.addHost(name='h1',
-                          ip='10.0.0.10/24',
-                          defaultRoute='via 10.1.0.1')
-        
-        h2 = self.addHost(name='h2',
-                          ip='10.0.0.20/24',
-                          defaultRoute='via 10.1.0.1')
-        
-        h3 = self.addHost(name='h3',
-                          ip='10.1.0.10/24',
-                          defaultRoute='via 10.2.0.1')
-        
-        h4 = self.addHost(name='h4',
-                          ip='10.1.0.20/24',
-                          defaultRoute='via 10.2.0.1')
-        
-        h5 = self.addHost(name='h5',
-                          ip='10.2.0.10/24',
-                          defaultRoute='via 10.3.0.1')
-        
-        
-        h6 = self.addHost(name='h6',
-                          ip='10.2.0.20/24',
-                          defaultRoute='via 10.3.0.1')
-        
+        h1 = self.addHost(name='h1',ip='10.0.0.10/24',defaultRoute='via 10.1.0.1')
+        h2 = self.addHost(name='h2',ip='10.0.0.20/24',defaultRoute='via 10.1.0.1')
+        h3 = self.addHost(name='h3',ip='10.1.0.10/24',defaultRoute='via 10.2.0.1')
+        h4 = self.addHost(name='h4',ip='10.1.0.20/24',defaultRoute='via 10.2.0.1')
+        h5 = self.addHost(name='h5',ip='10.2.0.10/24',defaultRoute='via 10.3.0.1')
+        h6 = self.addHost(name='h6',ip='10.2.0.20/24',defaultRoute='via 10.3.0.1')
         # Add host-switch links
         self.addLink(h1, s1)
         self.addLink(h2, s1)
