5fc6ec6b1017c3fe275538fdc466f1e734424832
Min Hyeok Lee <minhyeok.lee@mail.utoronto.ca>
2022-04-01 06:50:32 -0400

    topo changed

--- OSPF/topo.py
+++ OSPF/topo.py
@@ -36,14 +36,18 @@ class NetworkTopo(Topo):
 
         self.addLink(s2, r2, intfName2='r2-eth0', params2={'ip': '10.1.0.1/24'})
         
-        self.addLink(s3, m, intfName2='r3-eth0', params2={'ip': '10.2.0.1/24'})
+        self.addLink(s3, m, intfName2='m-eth0', params2={'ip': '10.2.0.1/24'})
         
         # Add router-router link in a new subnet for the router-router connection
         self.addLink(r1, r2, intfName1='r1-eth1', intfName2='r2-eth1', params1={'ip': '10.100.0.1/24'}, params2={'ip': '10.100.0.2/24'})
         
-        self.addLink(r1, m, intfName1='r1-eth2', intfName2='m-eth1', params1={'ip': '10.101.0.1/24'}, params2={'ip': '10.101.0.2/24'})
         
-        self.addLink(r2, m,intfName1='r2-eth2', intfName2='m-eth2', params1={'ip': '10.102.0.1/24'}, params2={'ip': '10.102.0.2/24'})
+        
+        
+        
+        
+        self.addLink(r1, m, intfName1='r1-eth2', intfName2='m-eth1', params1={'ip': '10.101.0.1/24'}, params2={'ip': '10.101.0.2/24'})
+        self.addLink(r2, m, intfName1='r2-eth2', intfName2='m-eth2', params1={'ip': '10.102.0.1/24'}, params2={'ip': '10.102.0.2/24'})
         
         # Adding hosts specifying the default route
         h1 = self.addHost(name='h1',ip='10.0.0.10/24',defaultRoute='via 10.0.0.1')
@@ -85,6 +89,7 @@ def run():
 
     # type the following command in the mininet shell
     info(net['r1'].cmd("ip route add 10.1.0.0/24 via 10.100.0.2 dev r1-eth1"))
+    
     info(net['r1'].cmd("ip route add 10.2.0.0/24 via 10.101.0.2 dev r1-eth2"))
     
     info(net['r2'].cmd("ip route add 10.0.0.0/24 via 10.100.0.1 dev r2-eth1"))
