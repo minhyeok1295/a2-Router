c039a5d3c04ed4b261c6ebdf997f5f41742b5549
Min Hyeok Lee <minhyeok.lee@mail.utoronto.ca>
2022-04-01 06:53:32 -0400

    topo changed

--- OSPF/topo.py
+++ OSPF/topo.py
@@ -22,9 +22,9 @@ class LinuxRouter(Node):
 class NetworkTopo(Topo):
     def build(self, **_opts):
         # Add 2 routers in two different subnets
-        r1 = self.addHost('r1', cls=LinuxRouter, ip='10.0.0.1/24' )
-        r2 = self.addHost('r2', cls=LinuxRouter, ip='10.1.0.1/24')
-        m = self.addHost('m', cls=LinuxRouter, ip='10.2.0.1/24')
+        r1 = self.addHost('r1', cls=LinuxRouter, ip='10.1.0.1/24' )
+        r2 = self.addHost('r2', cls=LinuxRouter, ip='10.2.0.1/24')
+        m = self.addHost('m', cls=LinuxRouter, ip='10.0.0.1/24')
 
         # Add 2 switches
         s1 = self.addSwitch('s1')
@@ -32,11 +32,11 @@ class NetworkTopo(Topo):
         s3 = self.addSwitch('s3')
 
         # Add host-switch links in the same subnet
-        self.addLink(s1, r1, intfName2='r1-eth0', params2={'ip': '10.0.0.1/24'})
+        self.addLink(s1, r1, intfName2='r1-eth0', params2={'ip': '10.1.0.1/24'})
 
-        self.addLink(s2, r2, intfName2='r2-eth0', params2={'ip': '10.1.0.1/24'})
+        self.addLink(s2, r2, intfName2='r2-eth0', params2={'ip': '10.2.0.1/24'})
         
-        self.addLink(s3, m, intfName2='m-eth0', params2={'ip': '10.2.0.1/24'})
+        self.addLink(s3, m, intfName2='m-eth0', params2={'ip': '10.0.0.1/24'})
         
         # Add router-router link in a new subnet for the router-router connection
         self.addLink(r1, r2, intfName1='r1-eth1', intfName2='r2-eth1', params1={'ip': '10.100.0.1/24'}, params2={'ip': '10.100.0.2/24'})
@@ -50,13 +50,11 @@ class NetworkTopo(Topo):
         self.addLink(r2, m, intfName1='r2-eth2', intfName2='m-eth2', params1={'ip': '10.102.0.1/24'}, params2={'ip': '10.102.0.2/24'})
         
         # Adding hosts specifying the default route
-        h1 = self.addHost(name='h1',ip='10.0.0.10/24',defaultRoute='via 10.0.0.1')
+        h1 = self.addHost(name='h1',ip='10.1.0.10/24',defaultRoute='via 10.1.0.1')
         
         #h2 = self.addHost(name='h2', ip='10.0.0.20/24', defaultRoute='via 10.0.0.1')
         
-        h3 = self.addHost(name='h3',
-                          ip='10.1.0.10/24',
-                          defaultRoute='via 10.1.0.1')
+        h3 = self.addHost(name='h3', ip='10.2.0.10/24', defaultRoute='via 10.2.0.1')
         
         #h4 = self.addHost(name='h4', ip='10.1.0.20/24', defaultRoute='via 10.1.0.1')
         '''
@@ -88,15 +86,15 @@ def run():
     
 
     # type the following command in the mininet shell
-    info(net['r1'].cmd("ip route add 10.1.0.0/24 via 10.100.0.2 dev r1-eth1"))
+    info(net['r1'].cmd("ip route add 10.2.0.0/24 via 10.100.0.2 dev r1-eth1"))
     
-    info(net['r1'].cmd("ip route add 10.2.0.0/24 via 10.101.0.2 dev r1-eth2"))
+    info(net['r1'].cmd("ip route add 10.0.0.0/24 via 10.101.0.2 dev r1-eth2"))
     
-    info(net['r2'].cmd("ip route add 10.0.0.0/24 via 10.100.0.1 dev r2-eth1"))
-    info(net['r2'].cmd("ip route add 10.2.0.0/24 via 10.102.0.2 dev r2-eth2"))
+    info(net['r2'].cmd("ip route add 10.1.0.0/24 via 10.100.0.1 dev r2-eth1"))
+    info(net['r2'].cmd("ip route add 10.0.0.0/24 via 10.102.0.2 dev r2-eth2"))
     
-    info(net['m'].cmd("ip route add 10.0.0.0/24 via 10.101.0.1 dev m-eth1"))
-    info(net['m'].cmd("ip route add 10.1.0.0/24 via 10.102.0.1 dev m-eth2"))
+    info(net['m'].cmd("ip route add 10.1.0.0/24 via 10.101.0.1 dev m-eth1"))
+    info(net['m'].cmd("ip route add 10.2.0.0/24 via 10.102.0.1 dev m-eth2"))
     
     net.startTerms()
     CLI(net)
