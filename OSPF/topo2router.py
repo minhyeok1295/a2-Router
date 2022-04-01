f6d9eb1ae9a848a0c6a72a10a108bdce354bbc95
Min Hyeok Lee <minhyeok.lee@mail.utoronto.ca>
2022-04-01 06:49:42 -0400

    topo changed

--- OSPF/topo2router.py
+++ OSPF/topo2router.py
@@ -33,15 +33,15 @@ class NetworkTopo(Topo):
 
         # Add host-switch links in the same subnet
         
-        self.addLink(sm, m1, intfName2='r3-eth0', params2={'ip': '10.0.0.1/24'})
+        self.addLink(sm, m1, intfName2='m1-eth0', params2={'ip': '10.0.0.1/24'})
         self.addLink(s1, r1, intfName2='r1-eth0', params2={'ip': '10.1.0.1/24'})
         self.addLink(s2, r2, intfName2='r2-eth0', params2={'ip': '10.2.0.1/24'})
         
 
         # Add router-router link in a new subnet for the router-router connection
         
-        self.addLink(r1, m1, intfName1='r1-eth1', intfName2='m-eth1', params1={'ip': '10.11.0.1/24'}, params2={'ip': '10.11.0.2/24'})
-        self.addLink(r2, m1, intfName1='r2-eth1', intfName2='m-eth2', params1={'ip': '10.12.0.1/24'}, params2={'ip': '10.12.0.2/24'})
+        self.addLink(r1, m1, intfName1='r1-eth1', intfName2='m1-eth1', params1={'ip': '10.11.0.1/24'}, params2={'ip': '10.11.0.2/24'})
+        self.addLink(r2, m1, intfName1='r2-eth1', intfName2='m1-eth2', params1={'ip': '10.12.0.1/24'}, params2={'ip': '10.12.0.2/24'})
         
         self.addLink(r1, r2, intfName1='r1-eth2', intfName2='r2-eth2', params1={'ip': '10.100.0.1/24'}, params2={'ip': '10.100.0.2/24'})
 
