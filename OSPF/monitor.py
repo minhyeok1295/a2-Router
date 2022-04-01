c33612c1b939e31b5437f36ee5ad0d0ec78c3f4e
Min Hyeok Lee <minhyeok.lee@mail.utoronto.ca>
2022-04-01 03:37:39 -0400

    topo changed

--- OSPF/monitor.py
+++ OSPF/monitor.py
@@ -4,7 +4,8 @@ from router import *
 class Monitor(Router):
     def __init__(self, ip):
         super.__init__(ip)
-        
+    
+    def
         
         
         
--- OSPF/topo.py
+++ OSPF/topo.py
@@ -55,12 +55,12 @@ class NetworkTopo(Topo):
                      params1={'ip': '10.100.0.1/24'},
                      params2={'ip': '10.100.0.2/24'})
         
-        self.addLink(r1,
-                     r3,
-                     intfName1='r1-eth2',
-                     intfName2='r3-eth1',
-                     params1={'ip': '10.101.0.1/24'},
-                     params2={'ip': '10.101.0.2/24'})
+       # self.addLink(r1,
+       #              r3,
+       #              intfName1='r1-eth2',
+        #             intfName2='r3-eth1',
+       #              params1={'ip': '10.101.0.1/24'},
+       #              params2={'ip': '10.101.0.2/24'})
         
         
         self.addLink(r2,
@@ -116,12 +116,12 @@ def run():
 
     # type the following command in the mininet shell
     info(net['r1'].cmd("ip route add 10.1.0.0/24 via 10.100.0.2 dev r1-eth1"))
-    info(net['r1'].cmd("ip route add 10.2.0.0/24 via 10.101.0.2 dev r1-eth2"))
+    #info(net['r1'].cmd("ip route add 10.2.0.0/24 via 10.101.0.2 dev r1-eth2"))
     
     info(net['r2'].cmd("ip route add 10.0.0.0/24 via 10.100.0.1 dev r2-eth1"))
     info(net['r2'].cmd("ip route add 10.2.0.0/24 via 10.102.0.2 dev r2-eth2"))
     
-    info(net['r3'].cmd("ip route add 10.0.0.0/24 via 10.101.0.1 dev r3-eth1"))
+    #info(net['r3'].cmd("ip route add 10.0.0.0/24 via 10.101.0.1 dev r3-eth1"))
     info(net['r3'].cmd("ip route add 10.1.0.0/24 via 10.102.0.1 dev r3-eth2"))
     CLI(net)
     net.stop()
