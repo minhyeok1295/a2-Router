80a10a7102c06ef70f1c99f881ff7b3908f10fec
Min Hyeok Lee <minhyeok.lee@mail.utoronto.ca>
2022-04-01 12:50:12 -0400

    topo changed

--- simple_router/simple_router.py
+++ simple_router/simple_router.py
@@ -10,27 +10,13 @@ class SimpleRouter(Router):
     def receive(self): #wait for broadcast
         recv_data, addr = self.thread_sock.recvfrom(1024)
         data = pickle.loads(recv_data)
-        if (check_on_same_switch(self.ip, data['src_ip'])):
-            self.lock.acquire()
-            # set src ip as key, the ip where the message is coming from as value 
-            self.table.create_entry( data['src_ip'], addr[0])
-            self.table.add_neighbors(data['src_ip'], "host")
-            self.lock.release()
-            self.thread_sock.sendto(make_packet(self.ip,addr,'',0),addr)
-        else: #it is router
-            self.thread_sock.sendto(make_packet(self.ip,addr,'NA',0),addr)
-    
-    def send_attach(self, ip):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((ip, 8000))
-        s.send(make_packet(self.ip, ip, "cr", -1))
-        s.close()
         self.lock.acquire()
-        dip = ip.rpartition(".")[0]
-        self.table.create_entry(dip, ip)
-        self.table.add_neighbors(ip, "router")
+        # set src ip as key, the ip where the message is coming from as value 
+        self.table.create_entry( data['src_ip'], addr[0])
+        self.table.add_neighbors(data['src_ip'], "host")
         self.lock.release()
-        
+        self.thread_sock.sendto(make_packet(self.ip,addr,'',0),addr)
+    
     def open_server(self):
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server.bind((self.ip, 8000))
@@ -41,16 +27,7 @@ class SimpleRouter(Router):
             packet = conn.recv(4096)
             if len(packet) != 0:
                 data = pickle.loads(packet)
-                if (data['message'] == 'cr' and data['ttl'] == -1): #connecting router
-                    
-                    self.lock.acquire()
-                    # set src ip as key, the ip where the message is coming from as value 
-                    dip = data['src_ip'].rpartition(".")[0]
-                    self.table.create_entry(dip + ".0", data['src_ip'])
-                    self.table.add_neighbors(data['src_ip'], "router")
-                    self.lock.release()
-                
-                elif (data['message'] == 'exit'):
+                if (data['message'] == 'exit'):
                     break
                 else:
                     data['ttl'] -= 1
